9e7cf15238cdf095b69377bb8f5337d196ec44d3
leemarshall <lpdmarshal@gmail.com>
2011-07-08 20:46:26 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leeapp::Application.config.secret_token = 'e2bcbf63e054538d21a4900e501d85a988d0de0bc5d6249877403c05069952838a28e1dde3eb6d69908454b75b24d5b7285e0d11b1fec54fc893649a73c1b9ec'
